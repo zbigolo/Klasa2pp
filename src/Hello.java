72e314173614729545021226e8c0175b0388e5f1
zbigolo <zbigolo@gazeta.pl>
2022-11-29 12:22:57 +0100

    nowy plik

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    // psvm, main
+
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+
+        int liczba = 100;
+        final char znak = 'X';
+        int rozmiarbuta;
+
+
+    }
+}
